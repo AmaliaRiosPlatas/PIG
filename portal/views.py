42a1840102ac412b2568d9a886595e5020f1b4c9
Amalia Rios Platas <amaliariosp@gmail.com>
2023-04-14 18:40:09 -0300

    primer commit

---
+++ portal/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def servicios(request):
+    return render(request,'servicios.html')
+
+def adopciones(request):
+    return render(request,'adopciones.html')
+
+def contacto(request):
+    return render(request,'contacto.html')
+    
+
