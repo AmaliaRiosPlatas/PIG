16ea1bfde15557ff8fe11dbac07676c7ea90d5a5
Amalia Rios Platas <amaliariosp@gmail.com>
2023-05-21 17:48:22 -0300

    Agregando widgets al form

--- portal/forms.py
+++ portal/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 
 class ContactoForm(forms.Form):
-    nombre = forms.CharField(label = 'Nombre', max_length=30)
-    apellido = forms.CharField(label = 'Apellido', max_length=80)
-    email = forms.EmailField(label = 'Email de contacto')
-    asunto = forms.CharField(label = 'Asunto', max_length=1500)
-    comentario = forms.CharField(label = 'Ingrese su comentario')
+    """ cambio 21/5 """
+    nombre = forms.CharField(label = 'Nombre', max_length=30, widget=forms.TextInput(attrs={'class':'Formulario_input'}))
+    apellido = forms.CharField(label = 'Apellido', max_length=80, widget=forms.TextInput(attrs={'class':'Formulario_input'}))  
+    email = forms.EmailField(label = 'Email de contacto', widget=forms.EmailInput(attrs={'class':'Formulario_input'}))
+    asunto = forms.CharField(label = 'Asunto', max_length=1500, widget=forms.TextInput(attrs={'class':'Formulario_input'}))
+    comentario = forms.CharField(label = 'Ingrese su comentario', widget=forms.TextInput(attrs={'class':'Formulario_input'}))
 
