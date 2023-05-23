07dbf580db3dbd5f6dbb2b6b2f6cc78913de722f
Amalia Rios Platas <amaliariosp@gmail.com>
2023-05-23 12:45:03 -0300

    Creación de modelos y cambios en el fomrs.py

--- portal/forms.py
+++ portal/forms.py
@@ -3,8 +3,13 @@ from django import forms
 class ContactoForm(forms.Form):
     """ cambio 21/5 """
     nombre = forms.CharField(label = 'Nombre', max_length=30, widget=forms.TextInput(attrs={'class':'Formulario_input'}))
-    apellido = forms.CharField(label = 'Apellido', max_length=80, widget=forms.TextInput(attrs={'class':'Formulario_input'}))  
+    apellido = forms.CharField(label = 'Apellido', max_length=80, widget=forms.TextInput(attrs={'class':'Formulario_input'})) 
+    telefono = forms.IntegerField(label = 'Teléfono', widget=forms.NumberInput(attrs={'class':'Formulario_input'})) 
+    domicilio = forms.CharField(label = 'Domicilio', max_length=100, widget=forms.TextInput(attrs={'class':'Formulario_input'}))
     email = forms.EmailField(label = 'Email de contacto', widget=forms.EmailInput(attrs={'class':'Formulario_input'}))
+    dni = forms.IntegerField(label = 'DNI', widget=forms.TextInput(attrs={'class':'Formulario_input'}))
+    mascota = forms.CharField(label = 'Nombre de Mascota', max_length=80, widget=forms.TextInput(attrs={'class':'Formulario_input'}))
     asunto = forms.CharField(label = 'Asunto', max_length=1500, widget=forms.TextInput(attrs={'class':'Formulario_input'}))
     comentario = forms.CharField(label = 'Ingrese su comentario', widget=forms.TextInput(attrs={'class':'Formulario_input'}))
+
 
--- portal/models.py
+++ portal/models.py
@@ -2,3 +2,47 @@ from django.db import models
 
 # Create your models here.
 
+class Veterinaria (models.Model):
+    nombre = models.CharField(max_length=30, verbose_name= 'Veterinaria')
+
+
+class Cliente (models.Model):
+    nombre = models.CharField(max_length=30, verbose_name= 'Nombre')
+    apellido = models.CharField(max_length=80, verbose_name= 'Apellido')
+    telefono = models.IntegerField(verbose_name= 'Teléfono')
+    domicilio = models.CharField(max_length=80, verbose_name= 'Domicilio')
+    email = models.CharField(max_length=30, verbose_name= 'Email de contacto')
+    dni = models.IntegerField(verbose_name= 'DNI')
+    mascota = models.CharField(max_length=30, verbose_name= 'Nombre de mascota')
+    veterinaria = models.ForeignKey(Veterinaria, on_delete= models.CASCADE)
+
+
+
+class Mascota (models.Model) :
+    id = models.IntegerField(verbose_name='ID')
+    raza = models.CharField(max_length=50, verbose_name= 'Raza')
+    edad = models.IntegerField(verbose_name= 'Edad')
+    tamaño = models.ImageField(verbose_name= 'Tamaño')
+    cliente = models.ForeignKey(verbose_name= 'Cliente', on_delete= models.CASCADE)
+
+
+class Servicios (models.Model) :
+    id = models.CharField(max_length=20, verbose_name= 'ID de Servicios')
+    mascotas = models.ManyToManyField(Mascota)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
